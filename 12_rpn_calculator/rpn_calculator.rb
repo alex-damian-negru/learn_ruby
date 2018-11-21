539cc129d0dd6fbd162a4d25f74ca67e69420f75
Alex Negru <alex.negru@agilefreaks.com>
2018-11-21 10:28:24 +0200

    Raise error when less than two values stacked away

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -8,24 +8,28 @@ class RPNCalculator
   end
 
   def plus
+    raise "calculator is empty" if array.length < 2
     result = array.last
     array.pop
     array[-1] += result
   end
 
   def minus
+    raise "calculator is empty" if array.length < 2
     result = array.last
     array.pop
     array[-1] -= result
   end
 
   def times
+    raise "calculator is empty" if array.length < 2
     result = array.last
     array.pop
     array[-1] *= result.to_f
   end
 
   def divide
+    raise "calculator is empty" if array.length < 2
     result = array.last
     array.pop
     array[-1] /= result.to_f
