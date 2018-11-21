3a7f908e57e4c3da95a69e7bb0c026c552f3fa3c
Alex Negru <alex.negru@agilefreaks.com>
2018-11-21 10:20:36 +0200

    Multiply and divide numbers

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -19,6 +19,18 @@ class RPNCalculator
     array[-1] -= result
   end
 
+  def times
+    result = array.last
+    array.pop
+    array[-1] *= result.to_f
+  end
+
+  def divide
+    result = array.last
+    array.pop
+    array[-1] /= result.to_f
+  end
+
   def value
     array.last
   end
