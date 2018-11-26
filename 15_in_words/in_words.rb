38026b9930886e514190e87e447c293d6728a7ed
Alex Negru <alex.negru@agilefreaks.com>
2018-11-26 14:18:33 +0200

    Read thousands

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -13,23 +13,18 @@ Fixnum.class_eval do
       "fifty", "sixty", "seventy", "eighty", "ninety"
     ]
 
-    get_under_100 = ->(n) { case n
-      when 11..19 
-        zero_to_nineteen[n]
-      else 
-        tens[n / 10] + (n % 10 == 0 ? "" : " " + zero_to_nineteen[n % 10]) 
+    get_under_hundred = ->(n) { case n
+      when 11..19 then zero_to_nineteen[n]
+      else tens[n / 10] + (n % 10 == 0 ? "" : " " + zero_to_nineteen[n % 10]) 
       end
     }
-    get_under_1000 = ->(n) { zero_to_nineteen[n / 100] + " hundred" + (n % 100  == 0 ? "" : " " + get_under_100.call(n % 100)) }
+    get_under_thousand = ->(n) { zero_to_nineteen[n / 100] + " hundred" + (n % 100  == 0 ? "" : " " + get_under_hundred.call(n % 100)) }
+    get_under_million = ->(n) { get_under_hundred[n / 1000].lstrip + " thousand" + (n % 1000  == 0 ? "" : " " + get_under_thousand.call(n % 1000)) }
 
-    if self < 20
-      zero_to_nineteen[self]
-
-    elsif self < 100
-      get_under_100.call(self)
-
-    elsif self < 1000
-      get_under_1000.call(self)
+    if    self < 20        then zero_to_nineteen[self]
+    elsif self < 100       then get_under_hundred.call(self)
+    elsif self < 1_000     then get_under_thousand.call(self)
+    elsif self < 1_000_000 then get_under_million.call(self)
     end
   end
 end
