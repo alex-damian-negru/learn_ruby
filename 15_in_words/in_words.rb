ee9aba19166eafd5edbd3aba98920bdf580ea892
Alex Negru <alex.negru@agilefreaks.com>
2018-11-26 13:51:08 +0200

    Read hundreds

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,29 +1,35 @@
 Fixnum.class_eval do
   def in_words
-    zero_to_nine = [
+    zero_to_nineteen = [
       "zero", "one", "two", "three", "four", 
-      "five", "six", "seven", "eight", "nine"
-    ]
-
-    ten_to_nineteen = [
+      "five", "six", "seven", "eight", "nine",
       "ten", "eleven", "twelve", "thirteen", 
       "fourteen", "fifteen", "sixteen", 
       "seventeen", "eighteen", "nineteen"
     ]
 
-    tens = [
-      "twenty", "thirty", "forty", "fifty",
-      "sixty", "seventy", "eighty", "ninety"
+    tens = [ 
+      "", "ten", "twenty", "thirty", "forty", 
+      "fifty", "sixty", "seventy", "eighty", "ninety"
     ]
 
-    if self < 10 
-      zero_to_nine[self]
-    elsif self >= 10 && self < 20
-      ten_to_nineteen[self % 10]
-    elsif self % 10 == 0
-      tens[self / 10 - 2]
-    else
-      tens[self % 10 - 2] + " " + zero_to_nine[self % 10]
+    get_under_100 = ->(n) { case n
+      when 11..19 
+        zero_to_nineteen[n]
+      else 
+        tens[n / 10] + (n % 10 == 0 ? "" : " " + zero_to_nineteen[n % 10]) 
+      end
+    }
+    get_under_1000 = ->(n) { zero_to_nineteen[n / 100] + " hundred" + (n % 100  == 0 ? "" : " " + get_under_100.call(n % 100)) }
+
+    if self < 20
+      zero_to_nineteen[self]
+
+    elsif self < 100
+      get_under_100.call(self)
+
+    elsif self < 1000
+      get_under_1000.call(self)
     end
   end
 end
