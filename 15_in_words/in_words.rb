9f4825843ee42ba3972f572bca60b9d472cdca05
Alex Negru <alex.negru@agilefreaks.com>
2018-11-28 10:05:25 +0200

    Extract methods into a module

--- 15_in_words/in_words.rb
+++ 15_in_words/in_words.rb
@@ -1,4 +1,4 @@
-Fixnum.class_eval do
+module InWords
   def in_words
     zero_to_nineteen = [
       "zero", "one", "two", "three", "four", 
@@ -22,15 +22,15 @@ Fixnum.class_eval do
     }
 
     get_hundreds  = ->(n) {
-      if n / 10**2 > 0 
-        zero_to_nineteen[n / 10**2] + " hundred" + (n % 10**2  == 0 ? "" : " " + get_tens.call(n % 10**2))
+      if n / 100 > 0 
+        zero_to_nineteen[n / 100] + " hundred" + (n % 100  == 0 ? "" : " " + get_tens.call(n % 100))
       else
-        get_tens[n % 10**2]
+        get_tens[n % 100]
       end
     }
 
     get_thousands = ->(n) {
-      if n / 10**3 >= 10**2
+      if n / 10**3 >= 100
         get_hundreds.call(n / 10**3) + " thousand" + (n % 10**3 == 0 ? "" : " " + get_hundreds.call(n % 10**3))
       elsif n / 10**3 > 0
         get_tens.call(n / 10**3).lstrip + " thousand" + (n % 10**3 == 0 ? "" : " " + get_hundreds.call(n % 10**3))
@@ -40,7 +40,7 @@ Fixnum.class_eval do
     }
 
     get_millions  = ->(n) {
-      if n / 10**6 >= 10**2
+      if n / 10**6 >= 100
         get_hundreds.call(n / 10**6) + " million" + (n % 10**6  == 0 ? "" : " " + get_thousands.call(n % 10**6))
       elsif n / 10**6 > 0
         get_tens.call(n / 10**6).lstrip + " million" + (n % 10**6 == 0 ? "" : " " + get_thousands.call(n % 10**6))
@@ -50,7 +50,7 @@ Fixnum.class_eval do
     }
 
     get_billions  = ->(n) {
-      if n / 10**9 >= 10**2
+      if n / 10**9 >= 100
         get_hundreds.call(n / 10**9) + " billion" + (n % 10**9  == 0 ? "" : " " + get_millions.call(n % 10**9))
       elsif n / 10**9 > 0
         get_tens.call(n / 10**9).lstrip + " billion"  + (n % 10**9 == 0 ? "" : " " + get_millions.call(n % 10**9))
@@ -60,7 +60,7 @@ Fixnum.class_eval do
     }
 
     get_trillions = ->(n) {
-      if n / 10**12 >= 10**2
+      if n / 10**12 >= 100
         get_hundreds.call(n / 10**12) + " trillion" + (n % 10**12  == 0 ? "" : " " + get_billions.call(n % 10**12))
       elsif n / 10**12 > 0
         get_tens.call(n / 10**12).lstrip + " trillion"  + (n % 10**12 == 0 ? "" : " " + get_billions.call(n % 10**12))
@@ -70,7 +70,7 @@ Fixnum.class_eval do
     }
 
     if    self < 20     then zero_to_nineteen[self]
-    elsif self < 10**2  then get_tens.call(self)
+    elsif self < 100    then get_tens.call(self)
     elsif self < 10**3  then get_hundreds.call(self)
     elsif self < 10**6  then get_thousands.call(self)
     elsif self < 10**9  then get_millions.call(self)
@@ -78,4 +78,8 @@ Fixnum.class_eval do
     elsif self < 10**15 then get_trillions.call(self)
     end
   end
+end
+
+Fixnum.class_eval do
+  include InWords
 end
