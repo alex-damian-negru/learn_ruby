75e33d142ae239455a2fbf28dd9242d25ca3c9b6
Alex Negru <alex.negru@agilefreaks.com>
2018-12-03 17:04:18 +0200

    Apply strategy pattern

--- 12_rpn_calculator/rpn_calculator.rb
+++ 12_rpn_calculator/rpn_calculator.rb
@@ -1,42 +1,38 @@
+# Context
 class RPNCalculator
-  def array
-    @array ||= []
+  attr_reader :array
+  attr_accessor :strategy
+
+  def initialize
+    @array = []
   end
 
   def push(number)
     array << number
   end
 
+  def value
+    array.last
+  end
+
   def plus
-    raise "calculator is empty" if array.length < 2
-    result = array.last
-    array.pop
-    array[-1] += result
+    set_operation(:+)
+    execute_operation
   end
 
   def minus
-    raise "calculator is empty" if array.length < 2
-    result = array.last
-    array.pop
-    array[-1] -= result
+    set_operation(:-)
+    execute_operation
   end
 
   def times
-    raise "calculator is empty" if array.length < 2
-    result = array.last
-    array.pop
-    array[-1] *= result
+    set_operation(:*)
+    execute_operation
   end
 
   def divide
-    raise "calculator is empty" if array.length < 2
-    result = array.last
-    array.pop
-    array[-1] /= result.to_f
-  end
-
-  def value
-    array.last
+    set_operation(:/)
+    execute_operation
   end
 
   def tokens(string)
@@ -51,19 +47,65 @@ class RPNCalculator
 
   def evaluate(string)
     tokens(string).map do |value|
-      value.is_a?(Integer) ? array.push(value) : do_operation(value)
+      if value.is_a?(Integer)
+        array.push(value)
+      else
+        set_operation(value)
+        execute_operation
+      end
     end
     result = array.join.to_f
     @array.clear
     result
   end
 
-  def do_operation(operation)
-    case operation
-    when :+ then plus   
-    when :- then minus
-    when :* then times
-    when :/ then divide
+  # TODO find out why plain `strategy` does not work
+  def set_operation(operation)
+    @strategy = case operation
+    when :+ then Addition.new   
+    when :- then Subtraction.new
+    when :* then Multiplication.new
+    when :/ then Division.new
     end
+  end
+
+  def execute_operation
+    raise "calculator is empty" if array.length < 2
+    val1 = array.pop
+    val2 = array.pop
+    array << @strategy.do_operation(val2, val1)
+  end
+end
+
+# Common interface for all strategies
+# EDIT: Is this needed?
+class Operation
+  def do_operation
+    raise NotImplementedError, 'Ask the subclass'
+  end
+end
+
+# Concrete strategies
+class Addition < Operation
+  def do_operation(val1, val2)
+    val1 + val2
+  end
+end
+
+class Subtraction < Operation
+  def do_operation(val1, val2)
+    val1 - val2
+  end
+end
+
+class Multiplication < Operation
+  def do_operation(val1, val2)
+    val1 * val2
+  end
+end
+
+class Division < Operation
+  def do_operation(val1, val2)
+    val1 / val2.to_f
   end
 end
